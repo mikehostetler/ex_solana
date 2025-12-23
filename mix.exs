138047a153ad4e1b45e202a4aa3f5557e23f1a88
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2025-12-23 07:40:08 -0600

    Squashed 'projects/req_llm/' changes from 13abb111c..b52d4787b
    b52d4787b docs: prevent manual CHANGELOG edits (#296)
    7250e09a3 chore: release version v1.2.0
    70770bd23 chore: update zoi dependency to version 0.14 and adjust schema metadata handling
    4eb984308 feat: add image generation support (#293)
    4963de570 ci: standardize quality alias and update to github-actions v3
    
    git-subtree-dir: projects/req_llm
    git-subtree-split: b52d4787b1e1a23720c032cf0e944b866ec88eea

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule ReqLLM.MixProject do
   use Mix.Project
 
-  @version "1.1.0"
+  @version "1.2.0"
   @source_url "https://github.com/agentjido/req_llm"
 
   def project do
@@ -169,7 +169,7 @@ defmodule ReqLLM.MixProject do
       {:splode, "~> 0.2.3"},
       {:typedstruct, "~> 0.5"},
       {:uniq, "~> 0.6"},
-      {:zoi, "~> 0.10"},
+      {:zoi, "~> 0.14"},
       {:jsv, "~> 0.11"},
       {:llm_db, "~> 2025.12"},
 
