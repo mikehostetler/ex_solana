d44318b7847af552307ab167033c78d401ca0858
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2025-12-23 15:43:23 -0600

    chore: add both depot and hako to workspace config

--- config/workspace.exs
+++ config/workspace.exs
@@ -61,6 +61,13 @@ config :jido_workspace,
       path: "projects/jido_workbench"
     },
     %{
+      name: "depot",
+      upstream_url: "git@github.com:agentjido/depot.git",
+      branch: "master",
+      type: :library,
+      path: "projects/depot"
+    },
+    %{
       name: "hako",
       upstream_url: "git@github.com:agentjido/hako.git",
       branch: "main",
