a47f221007a0e92e308e7d0af3f0598208874873
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-01-03 19:01:49 -0600

    Squashed 'projects/jido_sandbox/' content from commit aa57c826d
    git-subtree-dir: projects/jido_sandbox
    git-subtree-split: aa57c826d5e10ee9e20fa86b83e207e65881dba9

---
+++ lib/jido_sandbox.ex
@@ -0,0 +1,18 @@
+defmodule JidoSandbox do
+  @moduledoc """
+  Documentation for `JidoSandbox`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JidoSandbox.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
